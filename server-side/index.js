b4f1891a405f70683ce84d88d4a82b25b4b8fac8
mariemhawary <mariemhawary843@gmail.com>
2025-05-13 01:35:16 +0300

    edits

--- server-side/index.js
+++ server-side/index.js
@@ -52,6 +52,8 @@ app.use(
       "http://localhost:5173",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
