59fab9217504ddebc55bf90906aea617985a83ee
mariemhawary <mariemhawary843@gmail.com>
2025-05-13 11:18:08 +0300

    handel rewrites

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -3,13 +3,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', 
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')  
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
